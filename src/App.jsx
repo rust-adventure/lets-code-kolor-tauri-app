514ba71848ff8ff7527b058a717e38b00a559cf5
Christopher Biscardi <chris@christopherbiscardi.com>
2022-04-18 06:59:55 -0700

    kolor

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+import { SketchPicker } from "react-color";
+import { invoke } from "@tauri-apps/api/tauri";
+
+function App() {
+  const [color, setColor] = useState("#1fa9f4");
+  const [gradient, setGradient] = useState([]);
+
+  return (
+    <div>
+      <SketchPicker
+        color={color}
+        onChange={(color, event) => {
+          setColor(color);
+          invoke("generate_gradient", color.rgb).then((grad) => {
+            console.log(grad);
+            setGradient(grad);
+          });
+        }}
+      />
+      {gradient.map((color) => (
+        <div
+          style={{
+            padding: "2rem",
+            backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+          }}
+        >
+          rgb({color[0]}, {color[1]}, {color[2]})
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
